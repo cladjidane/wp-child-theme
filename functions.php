3546eb497a59f8a11f4fa08e11a9ba829a7efc86
Canu Fabien <fabien.canu@gmail.com>
2023-03-02 19:02:49 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -29,21 +29,21 @@ function wct_carousel( $atts ) {
 	// Attributes
 	$atts = shortcode_atts(
 		array(
-      'rubrique' => null
+      'slides' => null
 		),
     $atts
 	);
 
   ob_start();
+  $images = get_post_meta( $atts["slides"], 'Image' );
   ?>
 
   <div class="carousel-wrapper">
 
     <ul class="slides-container" id="slides-container">
-      <li class="slide"></li>
-      <li class="slide"></li>
-      <li class="slide"></li>
-      <li class="slide"></li>
+      <?php foreach ($images as $key => $value) : ?>
+      <li class="slide"><?php echo $value ?></li>
+      <?php endforeach; ?>
     </ul>
 
     <button class="slide-arrow" id="slide-arrow-prev">
@@ -59,3 +59,40 @@ function wct_carousel( $atts ) {
 	return ob_get_clean();
 }
 add_shortcode( 'wct_carousel', 'wct_carousel' );
+
+/**
+ * Custom post type : Slides
+ */
+function wct_cpt() {
+	$labels = array(
+		'name'                => "Slide",
+		'singular_name'       => "Slides",
+    'add_new_item' => 'Ajouter un slide',
+    'edit_item' => 'Modifier un slide',
+    'menu_name' => 'Slides'
+	);
+	$args = array(
+		'label'               =>"Slide",
+		'description'         => "Slides",
+		'labels'              => $labels,
+    'public' => true,
+    'show_in_rest' => true,
+    'has_archive' => true,
+    'supports' => array( 'title', 'custom-fields' ),
+		'capability_type'     => 'page',
+    'menu_position' => 5,
+    'menu_icon' => 'dashicons-admin-customizer',
+	);
+	register_post_type('slide', $args);
+}
+
+add_action('init', 'wct_cpt', 10);
+
+/**
+ * Désactiver Guttenberg dans certains CPT
+ */
+function wct_disable_gutenberg($current_status, $post_type) {
+  if ($post_type === 'slide') return false;
+  return $current_status;
+}
+add_filter('use_block_editor_for_post_type', 'wct_disable_gutenberg', 10, 2);
